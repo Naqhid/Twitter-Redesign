15c3ee01f8febc743f36ad8e411251938792ee13
bren2102 <brb.yg.21@gmail.com>
2020-04-23 17:15:22 -0500

    Create sigup view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   #get 'sessions/new'
   get '/login'  , to: 'sessions#new'
+  root 'sessions#new'
   post'/login'          , to: 'sessions#create'
   delete '/logout'      , to: 'sessions#destroy'
   get '/home'    , to:  'opinions#index'
+  get '/signup'   , to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :show]
   resources :opinions, only: [:create, :show, :index]
